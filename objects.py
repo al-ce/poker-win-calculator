32268a0deb68bc588a1787d7cbf8dc21a4815f0b
Alessandro Ceserani <aceserani@gmail.com>
2022-10-31 12:26:44 -0500

    all hands return as a list of tuples
    In the format [([Card], rank)] or [([Card1, Card2, ...], rank)] or in the case of a full house or two pair, [([Card1, Card2], rank), ([Card1, Card2, Card3], rank)]

--- objects.py
+++ objects.py
@@ -5,9 +5,7 @@ from random import shuffle
 # TODO: allow user to choose symbols or letters for suits and maybe mess with
 # ANSI escape sequences for colors
 # suits = ["\u2664", "\u2661", "\u2662", "\u2667"]
-suits = ["C", "D", "S", "H"]
 # suits = ["\033[94mC\033[00m", "\033[91mD\033[00m", "\033[94mS\033[00m", "\033[91mH\033[00m"]
-rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 # Helper functions
 # def prRed(skk):
@@ -98,6 +96,9 @@ class Card:
 
 
 class Deck:
+    rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    suits = ["C", "D", "S", "H"]
+
     def __init__(self):
         self.__cards = self.initialize_deck()
 
@@ -106,10 +107,12 @@ class Deck:
         return self.__cards
 
     def initialize_deck(self):
+        """Return a shuffled deck (randomized list of Cards). Called on each
+        round."""
         deck = []
-        for suit in suits:
+        for suit in self.suits:
             for i in range(2, 15):
-                card = Card(suit, (rank[i - 2], i))
+                card = Card(suit, (self.rank[i - 2], i))
                 deck.append(card)
         shuffle(deck)
         return deck
@@ -144,7 +147,7 @@ class Dealer:
     streets = {"3": "Flop", "4": "Turn", "5": "River"}
 
     def __init__(self, players: list):
-        self.__deck = self.get_new_deck().cards
+        self.__deck = self.get_new_deck()
         # List of Player() objects
         self.__players = [player for player in players]
         self.__community_cards = []
@@ -156,10 +159,14 @@ class Dealer:
         return self.__deck
 
     def burn_card(self):
+        """Remove a Card object from the deck. Called when 'burning' a card on
+        each street, i.e. when the dealer deals the flop, turn and river."""
         self.deck.pop()
         return
 
     def deal_card(self, location=0):
+        """Returns a Card object. Called when dealing either the hole cards to a
+        Player or when adding to the community cards."""
         card = self.deck.pop()
         card.location = location
         # Place card at the "bottom" of the deck so it can be used in
@@ -169,20 +176,25 @@ class Dealer:
         return card
 
     def deal_flop(self):
+        """Call deal_to_community_cards() three times (i.e. a flop) and
+        burn_card()"""
         self.burn_card()
         [self.deal_to_community_cards() for i in range(3)]
         return
 
     def deal_river(self):
-        self.burn_card()
+        """Call deal_to_community_cards() one time (i.e. a flop) and
+        burn_card()."""
         self.deal_to_community_cards()
         return
 
     def deal_to_community_cards(self):
+        """Append one Card to the community_cards list."""
         card = self.deal_card()
         self.__community_cards.append(card)
 
     def deal_to_players(self):
+        """Deal two Cards to a Player."""
         for player in self.__players:
             hole = []
             for i in range(2):
@@ -192,14 +204,19 @@ class Dealer:
         return
 
     def deal_turn(self):
+        """Call deal_to_community_cards() one time (i.e. a flop) and
+        burn_card()."""
         self.burn_card()
         self.deal_to_community_cards()
         return
 
     def get_new_deck(self):
-        return Deck()
+        """Returns a shuffled Deck of Cards."""
+        return Deck().cards
 
     def show_community_cards(self):
+        # NOTE: Currently unused
+        """Print community_cards list."""
         cards = self.__community_cards
         print(f"{self.which_street(len(cards)):8}:", end="")
         [print(f" {card.id}", end="") for card in cards]
@@ -207,6 +224,8 @@ class Dealer:
         return
 
     def show_all_players_hands(self):
+        # NOTE: Currently unused
+        """Print all Player's hole cards."""
         for player in self.__players:
             print(f"Player {player.id}:", end="")
             [print(f" {card.id:2}", end="") for card in player.hole]
@@ -214,6 +233,8 @@ class Dealer:
         return
 
     def show_remaining_cards(self):
+        # NOTE: Currently unused
+        """Print all undealt cards in the Deck"""
         [print(card.id) for card in self.deck]
         return
 
@@ -223,9 +244,13 @@ class Dealer:
 
 
 class HandRanker:
+    """Takes a Player object and a deck of dealt cards and calculates the
+    Player's valid hands and ranks them."""
+
     # This object should only exist after a flop and it should not persist
     # for more than one street to prevent duplicate counts.
 
+    # A dictionary of possible match types with their length as their keys.
     match_dict = {2: "pair", 3: "set", 4: "quads"}
 
     # @param deck: list of all Card objects minus burned ones
@@ -237,11 +262,23 @@ class HandRanker:
         self.__player = player
         self.__deck = deck
         self.__hole = player.hole
-        self.__high_card = player.hole[0], player.hole[0].rank
-        self.__kicker = player.hole[1], player.hole[1].rank
         self.__comm_cards = [card for card in self.deck if card.location == 0]
         self.__dealt = sorted(self.hole + self.comm_cards, key=get_rank, reverse=True)
 
+        # Each hand is formatted as a list of tuples. The first element of the
+        # tuple is a list of the cards in that hand, and the second item is the
+        # rank of that list as an int.. The rank is either the rank/value of
+        # the high card, kicker, or match type.
+        # e.g. a high card of King, a pair, set, or quads of Kings, a King high
+        # straight of flush, all would be a tuple with a list of the
+        # corresponding cards plus an int of 13
+        # e.g., ([KH, 13]) or ([KH, KC], 13), or
+        # ([9H, 10C, JC, QS, KC], 13) etc.
+        # A full house or a two pair would be a list of two tuples in
+        # the format [([set/pair], rank), ([pair], rank)].
+
+        self.__high_card = [([player.hole[0]], player.hole[0].rank)]
+        self.__kicker = [([player.hole[1]], player.hole[1].rank)]
         # Matches made by the table, either a pair in hand, or pairs, sets,
         # quads made with or by the community cards
         self.__table_matches = self.matches_check(self.dealt)
@@ -250,8 +287,7 @@ class HandRanker:
         self.__table_highest_matches = (
             self.get_highest_matches(self.table_matches) if self.table_matches else None
         )
-
-        self.__high_pair = self.check_pair()
+        self.__high_pair = self.check_high_pair()
         self.__low_pair = self.check_low_pair()
         self.__two_pair = self.check_two_pair()
         self.__set = self.check_set()
@@ -265,16 +301,20 @@ class HandRanker:
         self.__royal_fush = self.check_royal_flush()
 
     def __repr__(self) -> str:
-        # TODO: make some functions to help you format these correctly
         hbr = self.hands_by_rank()
-        message = f"{'Board:':12}{str(self.comm_cards)}\n"\
-                f"{'Hole Cards:':12}{str(self.hole)}\n"
+        message = (
+            f"{'Board:':12}{str(self.comm_cards)}\n"
+            f"{'Hole Cards:':12}{str(self.hole)}\n"
+        )
         if not hbr:
             return f"Player {self.player.id} made no hands."
+
         message += f"{'P'+str(self.player.id)+' hands':^20}\n"
         for k, v in hbr.items():
             message += f"{k+':':11} "
-            message += str(v[0][0])
+            # message += str([hand[0] for hand in v[0]])
+            for hand in v[0]:
+                message += str(hand[0])
             message += "\n"
 
         return message
@@ -359,28 +399,11 @@ class HandRanker:
     def table_highest_matches(self):
         return self.__table_highest_matches
 
-    def check_pair(self):
-        thm = self.table_highest_matches
-        if thm and "pair" in thm:
-            return thm["pair"]
-        return
-
-    def check_low_pair(self):
-        thm = self.table_highest_matches
-        if thm and "low pair" in thm:
-            return thm["low pair"]
-        return
-
-    def check_set(self):
-        thm = self.table_highest_matches
-        if thm and "set" in thm:
-            return thm["set"]
-        return
-
-    def check_quads(self):
-        thm = self.table_highest_matches
-        if thm and "quads" in thm:
-            return thm["quads"]
+    def check_flush(self):
+        for cards in self.suited.values():
+            # If the length (cards[0]) of the suit count is 5, return the flush
+            if cards[0] == 5:
+                return (cards[1],)
         return
 
     def check_full_house(self):
@@ -390,107 +413,41 @@ class HandRanker:
         set = self.set
         high_pair = self.high_pair
         if set and high_pair:
-            full_house = (high_pair, set)
+            full_house = [high_pair[0], set[0]]
         else:
             return
         return full_house
 
-    def check_two_pair(self):
+    def check_low_pair(self):
         thm = self.table_highest_matches
-        if not thm:
-            return
-        if self.low_pair and self.high_pair:
-            two_pair = (self.high_pair, self.low_pair)
-        else:
-            return
-        return two_pair
+        if thm and "low pair" in thm:
+            return thm["low pair"]
+        return
 
-    def hands_by_rank(self):
-        # TODO: Change the data types for some of these so they're more easily
-        # accessible. e.g. nested dicts? might be bettes than nested tuples
-        hands = {
-            "Kicker": (self.kicker, 0),
-            "High Card": (self.high_card, 1),
-            "Pair": (self.high_pair, 2),
-            "Two Pair": (self.two_pair, 3),
-            "Set": (self.set, 4),
-            "Straight": (self.straight, 5),
-            "Flush": (self.flush, 6),
-            "Full House": (self.full_house, 7),
-            "Quads": (self.quads, 8),
-            "Straight Flush": (self.straight_flush, 9),
-            "Royal Flush": (self.royal_flush, 10),
-        }
+    def check_high_pair(self):
+        thm = self.table_highest_matches
+        if thm and "pair" in thm:
+            return [thm["pair"]]
+        return
 
-        # Only return actual winning hands, not "None".
-        hands = {k: hands[k] for k, v in hands.items() if v[0]}
-        return hands
+    def check_quads(self):
+        thm = self.table_highest_matches
+        if thm and "quads" in thm:
+            return (thm["quads"],)
+        return
 
-    def get_highest_matches(self, matches: dict):
-        """From a dict of all matches, return a dict of only the highest and the low pair."""
-        temp = {}
-        for rank, card_data in matches.items():
-            cards = card_data[0]
-            match_type = card_data[1]
+    def check_royal_flush(self):
+        sf = self.straight_flush
+        if sf and sf[0].rank == 14:
+            # Get rid of straight flush to avoid redundant hands.
+            self.__straight_flush = None
+            return sf
+        return
 
-            # If we haven't entered this match yet, enter it and continue.
-            if match_type not in temp:
-                temp[match_type] = cards, rank
-                continue
-
-            # If we have entered it but its rank is lower, set it as the low
-            # pair unless it's quads, which makes it irrelevant.
-
-            # NOTE: If the "low pair" is a pair, the length of the list of its
-            # cards will be 2, but 3 if the "low pair" is a set (e.g. if the
-            # player makes two sets with the boards high and low pair)
-
-            if rank <= temp[match_type][1]:
-                if match_type != "quads":
-                    temp["low pair"] = cards, rank
-        return temp
-
-    def matches_check(self, cards: list, *args: list):
-        """Check for which cards match in a list. If more than one list is
-        passed to the function, check which cards from those lists match the
-        rank of the original list."""
-
-        match_types = ["pair", "set", "quads"]
-        matches = {}
-
-        for card in cards:
-            rank = card.rank
-
-            if rank in matches:
-                matches[rank][0].append(card)
-            else:
-                matches[rank] = [[card], ""]
-
-            # Update the type of match based on length of matching cards
-            cards = matches[rank][0]
-            matches[rank][1] = match_types[len(cards) - 2]
-
-            if not args:
-                continue
-
-            card_lists = [card for arg in args for card in arg]
-            for other_card in card_lists:
-                if other_card.rank == rank and other_card not in matches[rank][0]:
-                    matches[rank][0].append(other_card)
-
-            # Update the type of match based on length of matching cards
-            matches[rank][1] = match_types[len(cards) - 2]
-
-        # Return a copy of matches dict with only keys with list of len 2+
-        matches = {
-            key: matches[key] for key, cards in matches.items() if len(cards[0]) > 1
-        }
-        return matches
-
-    def is_ace_five_straight(self, card_ranks: list):
-        ace_five = [2, 3, 4, 5, 14]
-        if sorted(card_ranks) == ace_five:
-            return True
+    def check_set(self):
+        thm = self.table_highest_matches
+        if thm and "set" in thm:
+            return [thm["set"]]
         return
 
     def check_straight(self):
@@ -526,9 +483,9 @@ class HandRanker:
         if len(straights) > 1:
             g = lambda cards: sum([card.rank for card in cards])
             highest_straight = sorted(straights, key=g, reverse=True)[0]
-            return highest_straight
+            return (highest_straight,)
         elif straights:
-            return straights[0]
+            return (straights[0],)
         return
 
     def check_straight_flush(self):
@@ -539,18 +496,21 @@ class HandRanker:
             return self.straight
         return
 
-    def check_royal_flush(self):
-        sf = self.straight_flush
-        if sf and sf[0].rank == 14:
-            # Get rid of straight flush to avoid redundant hands.
-            self.__straight_flush = None
-            return sf
-        return
+    def check_two_pair(self):
+        thm = self.table_highest_matches
+        if not thm:
+            return
+        if self.low_pair and self.high_pair:
+            two_pair = [self.high_pair[0], self.low_pair]
+        else:
+            return
+        return two_pair
 
     def count_suited_cards(self):
         suit_count = {}
         # Which suits does the player have in their hand?
         dealt_suits = [card.suit for card in self.dealt]
+        suits = ["C", "D", "S", "H"]
         for suit in suits:
             if suit in dealt_suits:
                 suited = [card for card in self.dealt if card.suit == suit]
@@ -563,12 +523,92 @@ class HandRanker:
                 suit_count[suit] = (length, suited)
         return suit_count
 
-    def check_flush(self):
-        for cards in self.suited.values():
-            # If the length (cards[0]) of the suit count is 5, return the flush
-            if cards[0] == 5:
-                return cards[1]
+    def get_highest_matches(self, matches: dict):
+        """From a dict of all matches, return a dict of only the highest and the low pair."""
+        temp = {}
+        for rank, card_data in matches.items():
+            cards = card_data[0]
+            match_type = card_data[1]
+
+            # If we haven't entered this match yet, enter it and continue.
+            if match_type not in temp:
+                temp[match_type] = cards, rank
+                continue
+
+            # If we have entered it but its rank is lower, set it as the low
+            # pair unless it's quads, which makes it irrelevant.
+
+            # NOTE: If the "low pair" is a pair, the length of the list of its
+            # cards will be 2, but 3 if the "low pair" is a set (e.g. if the
+            # player makes two sets with the boards high and low pair)
+
+            if rank <= temp[match_type][1]:
+                if match_type != "quads":
+                    temp["low pair"] = cards, rank
+        return temp
+
+    def hands_by_rank(self):
+        # TODO: Change the data types for some of these so they're more easily
+        # accessible. e.g. nested dicts? might be bettes than nested tuples
+        hands = {
+            "Kicker": (self.kicker, 0),
+            "High Card": (self.high_card, 1),
+            "Pair": (self.high_pair, 2),
+            "Two Pair": (self.two_pair, 3),
+            "Set": (self.set, 4),
+            "Straight": (self.straight, 5),
+            "Flush": (self.flush, 6),
+            "Full House": (self.full_house, 7),
+            "Quads": (self.quads, 8),
+            "Straight Flush": (self.straight_flush, 9),
+            "Royal Flush": (self.royal_flush, 10),
+        }
+
+        # Only return actual winning hands, not "None".
+        hands = {k: hands[k] for k, v in hands.items() if v[0]}
+        return hands
+
+    def is_ace_five_straight(self, card_ranks: list):
+        ace_five = [2, 3, 4, 5, 14]
+        if sorted(card_ranks) == ace_five:
+            return True
         return
+
+    def matches_check(self, cards: list, *args: list):
+        """Check for which cards match in a list. If more than one list is
+        passed to the function, check which cards from those lists match the
+        rank of the original list."""
+
+        match_types = ["pair", "set", "quads"]
+        matches = {}
+
+        for card in cards:
+            rank = card.rank
+
+            if rank in matches:
+                matches[rank][0].append(card)
+            else:
+                matches[rank] = [[card], ""]
+
+            # Update the type of match based on length of matching cards
+            cards = matches[rank][0]
+            matches[rank][1] = match_types[len(cards) - 2]
+
+            if not args:
+                continue
+            card_lists = [card for arg in args for card in arg]
+            for other_card in card_lists:
+                if other_card.rank == rank and other_card not in matches[rank][0]:
+                    matches[rank][0].append(other_card)
+
+            # Update the type of match based on length of matching cards
+            matches[rank][1] = match_types[len(cards) - 2]
+
+        # Return a copy of matches dict with only keys with list of len 2+
+        matches = {
+            key: matches[key] for key, cards in matches.items() if len(cards[0]) > 1
+        }
+        return matches
 
 
 def main(d: Dealer):
